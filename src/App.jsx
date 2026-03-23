a8d792cea5e531789dfc14f4c2795158fe7e9189
pedrohrdev <pedrohrdev@gmail.com>
2026-03-23 15:59:42 -0300

    chore: initialize portfolio with Vite and configure global CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+import './global.css'
+
+function App() {
+
+  return (
+
+    <Home />
+
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+// import Navbar from "../components/Navbar";
+// import Hero from "../components/Hero";
+// import About from "../components/About";
+// import Projects from "../components/Projects";
+// import Skills from "../components/Skills";
+// import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      {/* <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
